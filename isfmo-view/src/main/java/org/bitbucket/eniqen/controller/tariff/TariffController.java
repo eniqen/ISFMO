91f90e60552dd713ad75897556adbf9ad89d0382
Mikhail Nemenko <m.nemenko@white-soft.ru>
2016-01-09 15:43:23 +1000

    в контроллер тарифов добавил прокидывание ексепшена при ошибке

--- isfmo-view/src/main/java/org/bitbucket/eniqen/controller/tariff/TariffController.java
+++ isfmo-view/src/main/java/org/bitbucket/eniqen/controller/tariff/TariffController.java
@@ -1,10 +1,14 @@
 package org.bitbucket.eniqen.controller.tariff;
 
+import org.bitbucket.eniqen.exception.ExceptionInfoHandler;
 import org.bitbucket.eniqen.model.Tariff;
 import org.bitbucket.eniqen.service.TariffService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -12,13 +16,20 @@ import java.util.List;
  */
 
 @RestController
-@RequestMapping("/ajax/tariffs")
-public class TariffController {
+@RequestMapping(TariffController.REST_URL)
+public class TariffController extends ExceptionInfoHandler {
+
+    public static final String REST_URL = "/ajax/tariffs";
+
     @Autowired
     private TariffService tariffService;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST, headers = "accept=application/json")
-    public void save(@RequestBody Tariff tariff) {
+    public void save(@Valid @RequestBody Tariff tariff, BindingResult result, SessionStatus status) {
+        if (result.hasErrors()) {
+            throw LOG.getValidationException(result);
+        }
+        status.setComplete();
         if (tariff.getId() == 0) {
             tariff.setId(null);
         }
